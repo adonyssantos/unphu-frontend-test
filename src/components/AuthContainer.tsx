2a6dcaba66d83c39ee0afacbd804814f2d0feb5d
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-06-06 18:13:43 -0400

    Add AuthContainer component

---
+++ src/components/AuthContainer.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+
+interface AuthContainerProps {
+  children: ReactNode;
+  title: string;
+}
+
+export default function AuthContainer({ title, children }: AuthContainerProps) {
+  return (
+    <Flex bg='gray.100' align='center' justify='center' h='100vh'>
+      <Box bg='white' p={6} rounded='md'>
+        <Heading fontSize='xl' fontWeight={600} textAlign='center'>
+          {title}
+        </Heading>
+        {children}
+      </Box>
+    </Flex>
+  );
+}
